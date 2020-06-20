21afac2aff4d71eadb5b24cf245304ea55553e83
Juacorrea95 <juacorrea95@gmail.com>
2020-06-20 20:00:35 -0300

    timeout

--- gral.py
+++ gral.py
@@ -1,32 +1,34 @@
 
 import os
-import signal
+import threading as t
 from time import sleep
 
 encoding = 'utf-8'
 timeout_state = False
 
-def timeout_handler(signum, frame):
-    print ("\nTiempo de sesion finalizado. Presione Enter para continuar")
+def timeout_handler():
+    cls()
+    print ("> Tiempo de sesion finalizado. Presione Enter para continuar")
     global timeout_state
     timeout_state = True
     return
 
 def input_timeout(timeout : int) -> str:
-    signal.signal(signal.SIGALRM, timeout_handler)
-    signal.alarm(timeout)
+    #signal.signal(signal.SIGALRM, timeout_handler)
+    #signal.alarm(timeout)
+    timer = t.Timer(timeout, timeout_handler)
+    timer.start()
     global timeout_state
     timeout_state = False
     ans = input("Tiempo máximo de respuesta {} segundos." .format(timeout))
     if timeout_state == True:
         ans = ""
-        signal.alarm(0)
     return ans
 
 
 def cls():
-    None
-    #os.system('cls' if os.name == 'nt' else 'clear')
+    #None
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def pause():
     input(">>> Presione una tecla para continuar...")
@@ -42,4 +44,3 @@ def popUp( msg: str ):
     print(msg)
     waitms(2000)
     cls()
-
